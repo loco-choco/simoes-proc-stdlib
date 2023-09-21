38086068447d4931683d23644baf3a4cf20b452a
Locochoco <ivanrwpf@gmail.com>
2023-09-21 00:17:03 -0300

    implemented free and fixed bugs

--- memory-allocation/malloc.asm
+++ memory-allocation/malloc.asm
@@ -7,8 +7,11 @@ block_end_addr_pos : var #10
 
 ;---- Inicio do Programa Principal -----
 main:
-	loadn r7, #1001 ; 4000
-	loadn r6, #1011
+	; Configurando o espaco de memoria dinamica
+	loadn r7, #1001 ; addr inicial
+	loadn r6, #1011 ; addr final
+
+	;---------- Teste de Alocacao ate acabar ----------
 	call mem_move
 	call mem_init
 	loadn r7, #2
@@ -19,9 +22,37 @@ main:
 	call mem_alloc
 	; esperado r7 ter o valor de (1001 + 3 + 2) + 3 = 1009
 	breakp
-	loadn r7, #1
+	loadn r7, #4
 	call mem_alloc
+	breakp
 	; esperado r7 ter o valor de 0, pois nao ha mais espaco na memoria dinamica
+
+	;----------- Teste de Alocacao e liberacao de memoria ----------
+	call mem_init ; libera o bloco inteiro de memoria
+	loadn r7, #2
+	call mem_alloc
+	; esperado r7 ter o valor de 1001 + 3 = 1004
+	breakp
+	call mem_free
+	loadn r7, #2
+	call mem_alloc
+	; esperado r7 ter o valor de 1004 novamente, pois o bloco anterior foi limpo, e ele tem o tamanho exato
+	breakp
+	loadn r7, #2
+	call mem_alloc
+	; esperado r7 ter o valor de 1009
+	breakp
+	call mem_free
+	loadn r7, #2
+	call mem_alloc
+	; esperado r7 ter o valor de 1009 novamente
+	breakp
+	loadn r7, #1004
+	call mem_free ; libera 1004
+	loadn r7, #2
+	call mem_alloc
+	; esperado r7 ter o valorde 1004 agora
+	breakp
 	halt	; Fim do programa - Para o Processador
 	
 ;---- Fim do Programa Principal -----
@@ -65,7 +96,7 @@ mem_alloc:		; Rotina de alocacao dinamica de memoria
 	push r3 ; is_free
 	push r4 ; next_block
 	push r5 ; generic_alg
-
+	
 	loadn r0, #0
 	; Procurando espaco disponivel
 	load r1, block_end_addr_pos
@@ -85,7 +116,7 @@ mem_alloc_search_do_while:
 	sub r5, r4, r5 ; next_block - 3
 	sub r5, r5, r2 ; (next_block - 3) - pos
 	cmp r5, r7
-	jel mem_alloc_search_do_while ; (next_block - pos - 3) < allocated_size
+	jle mem_alloc_search_do_while ; (next_block - pos - 3) < allocated_size
 mem_alloc_search_do_while_end:
 	; if( next_block == NULL && (size - pos - 3) < allocated_size) return NULL
 	cmp r4, r0
@@ -100,12 +131,23 @@ mem_alloc_search_do_while_end:
 	jmp mem_alloc_return
 mem_alloc_mem_found:
 	; Temos um bloco de memoria livre que cabe, agora vemos se precisamos dividir ele
-	; if( (next_block - pos - 3) == size), o bloco eh perfeito, entao marcar como usado e retornar seu endereco
+	; if((next_block == NULL && ( block_end_addr - pos - 3 ) == size) || (next_block != NULL &&(next_block - pos - 3) == size))
+	; o bloco eh perfeito, entao marcar como usado e retornar seu endereco
+	cmp r0, r4
+	jne mem_alloc_size_if_not_end_block ; pula para a comparacao (next_block - pos - 3) == size, pois esse bloco nao eh o ultimo
+	loadn r5, #3
+	sub r5, r1, r5 ; block_end_addr - 3
+	sub r5, r5, r2 ; (block_end_addr - 3) - pos
+	cmp r5, r7
+	jne mem_alloc_split ; (block_end_adder - pos - 3) != allocated_size, entao temos que dividir em 2
+	jmp mem_alloc_return_current_block ; se tem tamanho igual, retornar esse bloco entao
+mem_alloc_size_if_not_end_block:
 	loadn r5, #3
 	sub r5, r4, r5 ; next_block - 3
 	sub r5, r5, r2 ; (next_block - 3) - pos
 	cmp r5, r7
-	jne mem_alloc_split; (next_block - pos - 3) != allocated_size, temos que quebrar esse bloco em 2
+	jne mem_alloc_split; (next_block - pos - 3) != allocated_size, entao temos que dividir em 2
+mem_alloc_return_current_block:
 	loadn r5, #3
 	storei r2, r5 ; seta pos[0] = 3, ou seja, nao ta livre
 	add r7, r2, r5 ; valor de retorno eh pos + 3
@@ -146,3 +188,18 @@ mem_alloc_return:
 	pop r1
 	pop r0
 	rts
+
+mem_free:		; Rotina de liberacao de memoria alocada
+				; Argumentos:
+				; r7 = memory_pointer, endereco da memoria alocada
+				; Retorno: nenhum
+	push r0
+	push r1
+	loadn r0, #0
+	loadn r1, #3
+	sub r1, r7, r1 ; Endereco da flag eh memory_pointer - 3
+	storei r1, r0 ; Seta a flag 'free' do bloco de memoria
+	pop r1
+	pop r0
+	rts
+
