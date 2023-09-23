f207d89bee3a19c062d3f88d949bb9d6550d802b
Locochoco <ivanrwpf@gmail.com>
2023-09-23 00:50:24 -0300

    added hex to int conversion

--- data-manipulation/string.asm
+++ data-manipulation/string.asm
@@ -2,10 +2,12 @@
 
 jmp main
 
-string_block : string "000000001"
+string_block : string "123"
+string2_block : string "ABC"
 
 ;---- Inicio do Programa Principal -----
 main:
+	; ------ Testando conversao em base 10 ------
 	loadn r7, #string_block
 	call string_lenght
 	; r7 deve ter o tamanho da string de 3
@@ -17,7 +19,39 @@ main:
 	breakp
 	loadn r7, #string_block
 	call convert_string_base_10_to_int
-	; r7 deve ter o valor da string de 100
+	; r7 deve ter o valor da string de 123
+	breakp
+	loadn r7, #string2_block
+	call string_lenght
+	; r7 deve ter o tamanho da string de 3
+	breakp
+	loadn r7, #string2_block ; pega o endereco da memoria com o char
+	loadi r7, r7 ; pega o char no endereco da memoria
+	call convert_char_base_10_to_int
+	; r6 deve ter o valor de 1, ja que A nao eh um algarismo na base 10
+	breakp
+	loadn r7, #string2_block
+	call convert_string_base_10_to_int
+	; r6 deve falhar, pois essa string nao tem algarismos na base 10
+	breakp
+	; ------ Testando conversao em base 16 ------
+	loadn r7, #string_block ; pega o endereco da memoria com o char
+	loadi r7, r7 ; pega o char no endereco da memoria
+	call convert_char_base_16_to_int
+	; r7 deve ter o valor do char '1' de 1
+	breakp
+	loadn r7, #string_block
+	call convert_string_base_16_to_int
+	; r7 deve ter o valor da string de 291
+	breakp
+	loadn r7, #string2_block ; pega o endereco da memoria com o char
+	loadi r7, r7 ; pega o char no endereco da memoria
+	call convert_char_base_16_to_int
+	; r7 deve ter o valor de 10
+	breakp
+	loadn r7, #string2_block
+	call convert_string_base_16_to_int
+	; r7 deve ter o valor de 2748
 	breakp
 	halt
 ; Fim do programa - Para o Processador
@@ -117,6 +151,111 @@ convert_string_base_10_to_int_loop:
 convert_string_base_10_to_int_overflow:
 	loadn r6, #2 ; codigo de overflow
 convert_string_base_10_to_int_return:
+	pop r5
+	pop r4
+	pop r3
+	pop r2
+	pop r1
+	pop r0
+	rts
+
+
+;----- TODO --------
+convert_char_base_16_to_int:	; Rotina de converter char para inteiro, base 16
+				; Argumentos:
+				; r7 = caractere para ser convertido
+				; Retorno:
+				; r7 = valor de retorno
+				; r6 = codigo de erro: 0 -> convertido; 1 -> nao eh um inteiro base 16
+
+	loadn r6, #'0' 
+	cmp r7, r6
+	jle convert_char_base_10_to_int_wrong_char ; r7 < '0'
+	loadn r6, #'9' ; usado para auxiliar nas comparacoes
+	cmp r7, r6
+	jel convert_char_base_16_to_int_0_9 ; r7 <= '9'
+	; r7 entre 0 e 9
+	loadn r6, #'A' 
+	cmp r7, r6
+	jle convert_char_base_10_to_int_wrong_char ; r7 < 'A'
+	loadn r6, #'F' ; usado para auxiliar nas comparacoes
+	cmp r7, r6
+	jel convert_char_base_16_to_int_A_F ; r7 <= 'F'
+	; r7 entre A e F
+	loadn r6, #'a' 
+	cmp r7, r6
+	jle convert_char_base_10_to_int_wrong_char ; r7 < 'a'
+	loadn r6, #'f' ; usado para auxiliar nas comparacoes
+	cmp r7, r6
+	jel convert_char_base_16_to_int_a_f ; r7 <= 'f'
+	; r7 entre A e F
+convert_char_base_16_to_int_wrong_char:
+	loadn r6, #1 ; o char nao eh de 0 - 9
+	rts
+convert_char_base_16_to_int_0_9:
+	loadn r6, #'0'
+	sub r7, r7, r6 ; r7 = r7 - '0' -> calculo de ascii de 0-9 para inteiro
+	jmp convert_char_base_16_to_int_return
+convert_char_base_16_to_int_A_F:
+	loadn r6, #10
+	add r7, r7, r6
+	loadn r6, #'A'
+	sub r7, r7, r6 ; r7 = r7 - 'A' + 10 -> calculo de ascii de A-F para inteiro
+	jmp convert_char_base_16_to_int_return
+convert_char_base_16_to_int_a_f:
+	loadn r6, #10
+	add r7, r7, r6
+	loadn r6, #'a' ;
+	sub r7, r7, r6 ; r7 = r7 - 'a' + 10 -> calculo de ascii de a-f para inteiro
+convert_char_base_16_to_int_return:
+	loadn r6, #0
+	rts
+
+convert_string_base_16_to_int:	; Rotina de converter string para inteiro.
+			; Argumentos: 
+			; r7 = pointeiro do inicio da string
+			; Retorno:
+			; r7 = valor convertido da string
+			; r6 = codigo de erro: 0 -> convertido; 1 -> nao eh um inteiro; 2 -> overflow no inteiro
+	push r0 ; 0
+	push r1 ; valor da saida atual
+	push r2 ; numero de casas a serem convertidas
+	push r3 ; valor da casa (16**r2)
+	push r4 ; ponteiro da string
+	push r5 ; 10
+
+	loadn r0, #0
+	loadn r1, #0
+	loadn r3, #1  ; valor da primeira casa decimal
+	loadn r5, #16 ; cada casa decimal tem valor de 16
+	mov r4, r7 ; r4 aponta para o inicio da string
+	call string_lenght ; temos agora o tamanho da string
+	mov r2, r7 ; que eh o numero de casas do numero
+	add r4, r4, r7 ; r4 (inicio + tamanho) aponta agora para o char de finalizacao da string ('\0)
+convert_string_base_16_to_int_loop:
+	dec r4 ; vai para o char anterior
+	mov r7, r4 ; pega o endero do char anterior
+	loadi r7, r7 ; pega o char do endereco apontado por r7
+	call convert_char_base_16_to_int
+	; em r7 temos o valor
+	; em r6 o codigo se o valor eh valido, se nao for (r6 != 0), retornar agora
+	cmp r6, r0
+	jne convert_string_base_16_to_int_return 
+	; r6 != 0, portanto o char nao eh um inteiro, retornemos agora com r6 setado
+	mul r7, r7, r3 
+	; valor_unidade = valor_unidade * valor_da_casa
+	add r1, r1, r7 ; valor_saida += valor_unidade
+	jov convert_string_base_16_to_int_overflow ; deu overflow na operacao anterior, retornar que o numero na string eh grande de mais
+	mul r3, r3, r5 ; valor_da_casa = valor_da_casa * 16, para termos o valor da casa seguinte que eh 16**casa
+	dec r2 ; reduz o numero de casa decimais a ser convertidas
+	cmp r2, r0
+	jne convert_string_base_16_to_int_loop ; while (casa_decimal != 0), pois quando o numero de casa decimais a ser convertidas for zero, entao nao tem mais unidades para somar
+	mov r7, r1 ; move o resultado das somas para r7
+	loadn r6, #0
+	jmp convert_string_base_16_to_int_return ; retorna o valor determinado
+convert_string_base_16_to_int_overflow:
+	loadn r6, #2 ; codigo de overflow
+convert_string_base_16_to_int_return:
 	pop r5
 	pop r4
 	pop r3
